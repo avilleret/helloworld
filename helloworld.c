df6b66dc8941a428fc78903ea0f0f5491892a087
Antoine Villeret <antoine.villeret@gmail.com>
2017-01-02 17:28:37 +0100

    loadbang issue

--- helloworld.c
+++ helloworld.c
@@ -1,27 +1,33 @@
-#include "m_pd.h"  
- 
-static t_class *helloworld_class;  
- 
-typedef struct _helloworld {  
-  t_object  x_obj;  
-} t_helloworld;  
- 
-void helloworld_bang(t_helloworld *x)  
-{  
-  post("Hello world!");  
-}  
- 
-void *helloworld_new(void)  
-{  
-  t_helloworld *x = (t_helloworld *)pd_new(helloworld_class);  
-  
-  return (void *)x;  
-}  
- 
-void helloworld_setup(void) {  
-  helloworld_class = class_new(gensym("helloworld"),  
-        (t_newmethod)helloworld_new,  
-        0, sizeof(t_helloworld),  
-        CLASS_DEFAULT, 0);  
-  class_addbang(helloworld_class, helloworld_bang);  
+#include "m_pd.h"
+
+static t_class *helloworld_class;
+
+typedef struct _helloworld {
+  t_object  x_obj;
+} t_helloworld;
+
+void helloworld_bang(t_helloworld *x)
+{
+  post("Hello world!");
+}
+
+void helloworld_loadbang(t_helloworld *x)
+{
+  post("loadbang ");
+}
+
+void *helloworld_new(void)
+{
+  t_helloworld *x = (t_helloworld *)pd_new(helloworld_class);
+
+  return (void *)x;
+}
+
+void helloworld_setup(void) {
+  helloworld_class = class_new(gensym("helloworld"),
+        (t_newmethod)helloworld_new,
+        0, sizeof(t_helloworld),
+        CLASS_DEFAULT, 0);
+  class_addbang(helloworld_class, helloworld_bang);
+  class_addmethod(helloworld_class, (t_method) helloworld_loadbang, gensym("loadbang"), A_NULL, 0);
 }
